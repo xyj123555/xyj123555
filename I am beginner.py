deb8da325f2f44c8c697badbd3496f22948d9e1a
xyj123555 <156784530+xyj123555@users.noreply.github.com>
2024-01-24 22:11:45 +0000

    Add files via upload

---
+++ I am beginner.py
@@ -0,0 +1,42 @@
+a=100
+b=50
+print(a)
+print(90)
+print(a*b)
+print('welcome to china')
+
+a=100
+b=50
+print(a,b)
+
+print('b')
+print(chr(98))
+
+print('[')
+print(chr(91))
+
+print(ord('北'))
+
+fp=open('note.text','w')
+print('welcome to beijing',file=fp)
+fp.close()
+
+print('Beijing',end='==')
+print('good')
+
+print('Beijing'+' is nice')
+
+name=input('enter your name:')
+print('my name is' + name)
+
+'''
+x=100
+print(x)
+'''
+#print(a)
+
+name=input('enter your name:')
+age=input('enter your age:')
+print('--self introduction--')
+print('my name is',name )
+print('my age is',age)
